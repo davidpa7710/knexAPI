48ad06ddc0bbef9fc2ad687599344f585528dc65
David Palacios <davidpa7710@gmail.com>
2022-06-20 19:26:28 -0500

    ✨  routes with express created

---
+++ controllers/homeController.js
@@ -0,0 +1,9 @@
+
+const homeCreate = (req, res) => {
+    res.send({message: 'Home Creado (FAKE)'})
+}
+
+module.exports = {
+    homeCreate
+} 
+    
---
+++ server.js
@@ -0,0 +1,18 @@
+const express = require('express')
+
+const app = express()
+
+const homeRoutes = require('./routes/homeRoutes')
+
+const PORT = 5050
+
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+/* Rutas */
+app.use('/api/v1', homeRoutes);
+
+app.listen(PORT, () => {
+    console.log(`Server On Port: ${PORT}`);
+})
+
